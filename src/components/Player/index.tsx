04813239ff060e2a3508dc4e8e0ac52683f408c2
Furkan INANC <hi@furkaninanc.com>
2021-04-05 00:18:15 +0300

    Add base Player component

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,53 @@
+import { forwardRef, memo, RefObject, useEffect } from 'react';
+
+import Plyr, { HTMLPlyrVideoElement, PlyrCallback } from 'plyr-react';
+import 'plyr-react/dist/plyr.css';
+
+interface IPlayer {
+  onPause: PlyrCallback;
+  onPlay: PlyrCallback;
+  onSeeked: PlyrCallback;
+  onTimeUpdate: PlyrCallback;
+  sources: string;
+  ref: RefObject<HTMLPlyrVideoElement>;
+}
+
+const Player: React.FC<IPlayer> = memo(
+  forwardRef(({ onPause, onPlay, onSeeked, onTimeUpdate, sources }, ref) => {
+    useEffect(() => {
+      // @ts-ignore
+      const plyr = ref?.current?.plyr;
+
+      const pauseEvent: PlyrCallback = onPause;
+      const playEvent: PlyrCallback = onPlay;
+      const seekedEvent: PlyrCallback = onSeeked;
+      const timeUpdateEvent: PlyrCallback = onTimeUpdate;
+
+      plyr?.on('pause', pauseEvent);
+      plyr?.on('play', playEvent);
+      plyr?.on('seeked', seekedEvent);
+      plyr?.on('timeupdate', timeUpdateEvent);
+
+      return () => {
+        plyr?.off('pause', pauseEvent);
+        plyr?.off('play', playEvent);
+        plyr?.off('seeked', seekedEvent);
+        plyr?.off('timeupdate', timeUpdateEvent);
+      };
+    }, [onPause, onPlay, onSeeked, onTimeUpdate, ref]);
+
+    return (
+      <>
+        <Plyr
+          ref={ref}
+          source={{
+            type: 'video',
+            sources: JSON.parse(sources),
+          }}
+        />
+      </>
+    );
+  })
+);
+
+export default Player;
