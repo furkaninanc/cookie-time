f58669c7d2f1cb04560d5ea1f4a2695f1a8c9372
Furkan INANC <hi@furkaninanc.com>
2021-04-05 03:21:08 +0300

    Update socket events for new backend

--- src/pages/RoomPage/index.tsx
+++ src/pages/RoomPage/index.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 
 import { HTMLPlyrVideoElement, PlyrCallback } from 'plyr-react';
 import socketIOClient, { Socket } from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
 
 import Input from '../../components/Input';
 import Player from '../../components/Player';
@@ -9,8 +10,9 @@ import logo from '../../logo.svg';
 import styles from './RoomPage.module.scss';
 
 interface IMessage {
-  owner: string;
   content: string;
+  id: string;
+  owner: string;
 }
 
 const RoomPage: React.FC = () => {
@@ -18,72 +20,147 @@ const RoomPage: React.FC = () => {
   const [socket, setSocket] = useState<Socket>();
   const [message, setMessage] = useState<string>('');
   const [messages, setMessages] = useState<IMessage[]>([]);
+  const [sources, setSources] = useState<string>(
+    JSON.stringify([
+      {
+        src: 'https://cdn.plyr.io/static/blank.mp4',
+        type: 'video/mp4',
+        size: 720,
+      },
+    ])
+  );
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [initialTime, setInitialTime] = useState<number>(0);
 
-  const addMessage = ({ content, owner }: IMessage) => {
+  const addMessage = ({ content, id, owner }: IMessage) => {
     setMessages((prevMessages) => {
       const newMessages = [...prevMessages];
       newMessages.push({
-        owner,
         content,
+        id,
+        owner,
       });
 
       return newMessages;
     });
   };
 
+  const addSystemMessage = useCallback((content: string) => {
+    addMessage({ content, id: uuidv4(), owner: '' });
+  }, []);
+
   useEffect(() => {
     setSocket(
-      socketIOClient('wss://furkaninanc.com', {
+      socketIOClient('http://localhost:7284', {
         transports: ['websocket'],
         upgrade: false,
         query: {
           room: 'lost',
-          username: 'furkan',
+          username: `furkan_${Math.random()}`,
         },
       })
     );
   }, []);
 
   useEffect(() => {
-    socket?.on('message', (content, owner) => {
-      addMessage({ content, owner });
+    if (playing && initialTime && ref?.current?.plyr?.currentTime) {
+      ref.current.plyr.currentTime = initialTime;
+      setInitialTime(0);
+    }
+  }, [initialTime, playing]);
+
+  useEffect(() => {
+    socket?.on('join', ({ time, video }) => {
+      setSources(
+        JSON.stringify([
+          {
+            src: video,
+            type: 'video/mp4',
+            size: 720,
+          },
+        ])
+      );
+
+      setInitialTime(time);
     });
 
-    socket?.on('stateChange', (state) => {
+    socket?.on('message:receive', ({ content, id, owner }) => {
+      addMessage({ content, id, owner });
+    });
+
+    socket?.on('member:join', ({ username }) => {
+      addSystemMessage(`${username} odaya katıldı`);
+    });
+
+    socket?.on('member:leave', ({ username }) => {
+      addSystemMessage(`${username} odadan ayrıldı`);
+    });
+
+    socket?.on('player:state', ({ state, username }) => {
       if (state === 1) {
         ref?.current?.plyr?.play();
+        addSystemMessage(`${username} videoyu başlattı`);
       } else {
         ref?.current?.plyr?.pause();
+        addSystemMessage(`${username} videoyu durdurdu`);
       }
     });
 
-    socket?.on('seek', (time) => {
-      ref!.current!.plyr!.currentTime! = time;
+    socket?.on('player:seek', ({ time, username }) => {
+      addSystemMessage(`${username} video zamanını değiştirdi`);
+
+      if (
+        ref?.current?.plyr?.currentTime &&
+        Math.abs(ref.current.plyr.currentTime - time) > 2
+      ) {
+        ref.current.plyr.currentTime = time;
+      }
     });
-  }, [socket]);
+
+    socket?.on('player:video', ({ video, username }) => {
+      addSystemMessage(`${username} videoyu değiştirdi`);
+
+      setSources(
+        JSON.stringify([
+          {
+            src: video,
+            type: 'video/mp4',
+            size: 720,
+          },
+        ])
+      );
+    });
+  }, [addSystemMessage, socket]);
 
   const onChange = (event: any) => setMessage(event.target.value);
   const onKeyDown = (event: any) => {
     if (event.keyCode === 13) {
-      socket?.emit('message', message);
+      if (message.startsWith('!video ')) {
+        const [, video] = message.split('!video ');
+        socket?.emit('player:video', { video });
+      } else {
+        socket?.emit('message:send', { content: message });
+      }
       setMessage('');
     }
   };
   const onPause: PlyrCallback = useCallback(() => {
-    socket?.emit('stateChange', false);
+    setPlaying(false);
+    socket?.emit('player:state', { state: 0 });
   }, [socket]);
   const onPlay: PlyrCallback = useCallback(() => {
-    socket?.emit('stateChange', true);
+    setPlaying(true);
+    socket?.emit('player:state', { state: 1 });
   }, [socket]);
   const onSeeked: PlyrCallback = useCallback(
     (event) => {
-      socket?.emit('seek', event.detail.plyr.currentTime);
+      socket?.emit('player:seek', { time: event.detail.plyr.currentTime });
     },
     [socket]
   );
   const onTimeUpdate: PlyrCallback = useCallback(
     (event) => {
-      socket?.emit('timeupdate', event.detail.plyr.currentTime);
+      socket?.emit('player:time', { time: event.detail.plyr.currentTime });
     },
     [socket]
   );
@@ -97,18 +174,7 @@ const RoomPage: React.FC = () => {
           onSeeked={onSeeked}
           onTimeUpdate={onTimeUpdate}
           ref={ref}
-          sources={JSON.stringify([
-            {
-              src: 'https://furkaninanc.com/minisync/lost/3x5.mp4',
-              type: 'video/mp4',
-              size: 720,
-            },
-            {
-              src: 'https://furkaninanc.com/minisync/lost/3x5.mp4',
-              type: 'video/mp4',
-              size: 1080,
-            },
-          ])}
+          sources={sources}
         />
       </div>
       <div className={styles.chatContainer}>
@@ -120,8 +186,8 @@ const RoomPage: React.FC = () => {
         </div>
         <div className={styles.messagesContainer}>
           {messages &&
-            messages.map(({ content, owner }) => (
-              <div className={styles.messageContainer}>
+            messages.map(({ content, id, owner }) => (
+              <div className={styles.messageContainer} key={id}>
                 <div className={styles.messageOwner}>{owner}</div>
                 <div className={styles.messageContent}>{content}</div>
               </div>
